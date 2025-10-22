e924310134268ebaa2490f56d7fcbbebb1958807
SuprajaGit654 <euonlinecareer@gmail.com>
2025-10-23 00:37:19 +0530

    Delete numberType.js

--- numberType.js
+++
@@ -1,8 +0,0 @@
-let num = -5
-if (num == 0) {
-    console.log("Number is zero")
-} else if (num > 0) {
-    console.log("Number is positive")
-} else if (num < 0) {
-    console.log("Number is negative")
-}
